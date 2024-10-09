82b96b899bf6e33e66eb1848f6685efe57be3bff
leozz009 <a00835604@tec.mx>
2024-10-09 15:41:04 -0600

    Initial Commit

---
+++ HealthWind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthWind
+//
+//  Created by Leonardo González on 09/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
