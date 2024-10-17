d5f7151ddeb9801f6f68de842908bd7e8666e8f6
leozz009 <a00835604@tec.mx>
2024-10-17 17:02:13 -0600

    Login and Darkmode

--- HealthWind/ViewComponents/Salud/BackgroundHealthViewComponent.swift
+++ HealthWind/ViewComponents/Salud/BackgroundHealthViewComponent.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct BackgroundHealthViewComponent: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.blueApp, Color.white]),
+            LinearGradient(gradient: Gradient(colors: [Color.blueApp, Color.black]),
                            startPoint: .bottom,
                            endPoint: UnitPoint(x: -0.1, y: 0.85))
             .frame(height: UIScreen.main.bounds.height)
--- HealthWind/Views/LoginView.swift
+++ HealthWind/Views/LoginView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct LoginView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        @State var email: String = ""
+        @State var password: String = ""
+        ZStack {
+            BackgroundViewComponent()
+            VStack{
+                Spacer()
+                HStack {
+                    Text("Iniciar sesión")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                        .font(.title)
+                        .foregroundColor(.red)
+                }.padding(.bottom,5)
+                
+                HStack{
+                    Text("Debes iniciar sesión para tener acceso a está funcionalidad")
+                        .padding(.bottom, 25)
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+                
+    
+                TextField("Correo electrónico", text: $email)
+                    .autocapitalization(.none)
+                    .keyboardType(.emailAddress)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.bottom, 20)
+                
+                SecureField("Contraseña", text: $password)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.bottom, 20)
+                
+                Button(action: {
+                    // Code
+                }) {
+                    Text("Iniciar Sesión")
+                        .bold()
+                        .padding()
+                        .background(Color.blueApp)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.top,8)
+                }
+                Spacer()
+            }.padding().padding()
+        }
     }
 }
 
