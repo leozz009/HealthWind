188c07c40185e86328155137a5e6981e56396cd2
leozz009 <a00835604@tec.mx>
2024-10-17 16:58:04 -0600

    Login and dark mode

---
+++ HealthWind/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  HealthWind
+//
+//  Created by Leonardo González on 17/10/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
