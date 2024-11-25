d9ef052d2fab5e2e5089c20121cd5757891d0472
Leonardo Adrián González Hernández <133685737+leozz009@users.noreply.github.com>
2024-11-25 11:16:48 -0600

    presentacion

--- HealthWind/ViewModels/Health/HealthViewModel.swift
+++ HealthWind/ViewModels/Health/HealthViewModel.swift
@@ -40,10 +40,7 @@ class HealthViewModel: ObservableObject {
 
                     }
                     
-                }
-            case .failure(let failure):
-                print(failure)
-            }
+    
         }
         self.loadedRecommendationes = true
     }
